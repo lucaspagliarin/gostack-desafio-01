598502e05647248139a9b240bc4130feee929784
unknown <lucas.pagliarin@gmail.com>
2019-06-29 15:25:59 -0300

    Created Creation Route

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 const projects = [
   {
     id: "1",
@@ -12,6 +14,19 @@ const projects = [
 
 server.get("/projects", (req, res) => {
   res.json(projects);
+});
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+  const project = {
+    id: id,
+    title: title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(projects);
 });
 
 server.listen(3333);
