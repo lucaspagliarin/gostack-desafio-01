64ace1b3f67892617712602514b8d03df701dfc7
unknown <lucas.pagliarin@gmail.com>
2019-06-29 15:20:42 -0300

    Added Get all Projects Route

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
+
+const projects = [
+  {
+    id: "1",
+    title: "Novo Projeto",
+    tasks: ["Nova Tarefa"]
+  }
+];
+
+server.get("/projects", (req, res) => {
+  res.json(projects);
+});
 
 server.listen(3333);
